3a944553ef232409a37d3e5019bbd8139247dcae
oysterjung <resd877@naver.com>
2024-06-02 18:43:11 +0900

    레포명 변경 및 오류내역 삭제
    ToonFlix -> Flutter_Practice로 레포명 변경
    
    오류내역 삭제

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:toonflix/widget/Button.dart';
 import 'package:toonflix/widget/currency_card.dart';
 
